07ca13003e0e8dec862476ad14e5931c978929de
Shirish Kamath <shirishk.87@gmail.com>
2023-07-29 15:56:46 +0530

    Package signing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,15 +1,21 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+val libVersion = "1.0.0-SNAPSHOT"
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+
+    id("com.vanniktech.maven.publish") version "0.25.3"
 }
 
 android {
     namespace = "com.dotslashlabs.media3.extractor.m4b"
-    version = "1.0.0-SNAPSHOT"
+    version = libVersion
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 16
+        minSdk = 24
     }
 
     buildTypes {
@@ -40,4 +46,37 @@ dependencies {
     api("androidx.media3:media3-container:$media3Version")
 
     api("androidx.annotation:annotation:1.6.0")
+}
+
+mavenPublishing {
+    coordinates("com.dotslashlabs", "media3-extractor-m4b", libVersion)
+
+    pom {
+        name.set("media3-extractor-m4b")
+        description.set("Provides m4b file format extractor including support for extracting chapter metadata.")
+        inceptionYear.set("2023")
+        url.set("https://github.com/dotslashlabs/media3-extractor-m4b")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("shirish87")
+                name.set("Shirish Kamath")
+                url.set("https://github.com/shirish87/")
+            }
+        }
+        scm {
+            url.set("https://github.com/dotslashlabs/media3-extractor-m4b")
+            connection.set("scm:git:git://github.com/dotslashlabs/media3-extractor-m4b.git")
+            developerConnection.set("scm:git:ssh://git@github.com/dotslashlabs/media3-extractor-m4b.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.S01)
+    signAllPublications()
 }
